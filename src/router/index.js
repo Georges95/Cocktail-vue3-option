27293fd1f91e4417c6fb170edc109efc0b2e10a2
georges <georges.laerche@gmail.com>
2023-11-23 19:49:52 +0100

    mise en place de la partie public et de son layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import Home from '@/views/public/Home.vue'
+import Cocktail from '@/views/public/Cocktail.vue'
+import Contact from '@/views/public/Contact.vue'
+
+import PublicLayout from '@/views/public/Layout.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'public',
+    component: PublicLayout,
+    children: [
+      { path: '/', name: 'home', component: Home },
+      { path: '/cocktails', name: 'cocktails', component: Cocktail},
+      { path: '/contact', name: 'contact', component: Contact}
+    ]
   },
+    
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/:pathMatch(.*)*', redirect: '/'
   }
+
+
 ]
 
 const router = createRouter({
